a2a8cd0d87acde04e171c9dbb275d4a7e082c11e
Pixselve <38262536+Pixselve@users.noreply.github.com>
2021-05-02 13:37:07 +0200

    :sparkles: login mutation

--- graph/schema.resolvers.go
+++ graph/schema.resolvers.go
@@ -5,9 +5,12 @@ package graph
 
 import (
 	"context"
+	"fmt"
+	"golang.org/x/crypto/bcrypt"
 	"kontrakt-server/graph/generated"
 	"kontrakt-server/graph/model"
 	"kontrakt-server/prisma/db"
+	"kontrakt-server/utils"
 )
 
 func (r *contractResolver) Skills(ctx context.Context, obj *model.Contract) ([]*model.Skill, error) {
@@ -18,15 +21,38 @@ func (r *contractResolver) Skills(ctx context.Context, obj *model.Contract) ([]*
 	var result []*model.Skill
 	for _, skill := range skills {
 		result = append(result, &model.Skill{
-			ContractID:    skill.ContractID,
-			ID:            skill.ID,
-			Name:          skill.Name,
+			ContractID: skill.ContractID,
+			ID:         skill.ID,
+			Name:       skill.Name,
 		})
 	}
 	return result, nil
 }
 
-func (r *queryResolver) Contracts(ctx context.Context) ([]*model.Contract, error) {
+func (r *mutationResolver) Login(ctx context.Context, username string, password string) (*model.AuthPayload, error) {
+	user, err := r.Prisma.User.FindUnique(db.User.Username.Equals(username)).Exec(ctx)
+	if err != nil {
+		return nil, err
+	}
+	err = bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password))
+	if err != nil {
+		return nil, fmt.Errorf("bad password")
+	}
+	token, err := utils.GetToken(user.Username)
+	if err != nil {
+		return nil, err
+	}
+	return &model.AuthPayload{
+		Token: token,
+		User: &model.User{
+			Username: user.Username,
+			Role:     model.Role(user.Role),
+		},
+	}, nil
+
+}
+
+func (r *queryResolver) Contracts(ctx context.Context, groupIds []int) ([]*model.Contract, error) {
 	contracts, err := r.Prisma.Contract.FindMany().Exec(ctx)
 	if err != nil {
 		return nil, err
@@ -45,11 +71,43 @@ func (r *queryResolver) Contracts(ctx context.Context) ([]*model.Contract, error
 	return result, err
 }
 
+func (r *queryResolver) Groups(ctx context.Context) ([]*model.Group, error) {
+	panic(fmt.Errorf("not implemented"))
+}
+
+func (r *queryResolver) Marks(ctx context.Context) ([]model.Mark, error) {
+	panic(fmt.Errorf("not implemented"))
+}
+
+func (r *queryResolver) Student(ctx context.Context, ownerUsername string) (*model.Student, error) {
+	panic(fmt.Errorf("not implemented"))
+}
+
+func (r *queryResolver) Contract(ctx context.Context, id int) (*model.Contract, error) {
+	panic(fmt.Errorf("not implemented"))
+}
+
+func (r *queryResolver) Students(ctx context.Context) ([]*model.Student, error) {
+	panic(fmt.Errorf("not implemented"))
+}
+
+func (r *queryResolver) Teachers(ctx context.Context) ([]*model.Teacher, error) {
+	panic(fmt.Errorf("not implemented"))
+}
+
+func (r *queryResolver) Me(ctx context.Context) (*model.User, error) {
+	panic(fmt.Errorf("not implemented"))
+}
+
 // Contract returns generated.ContractResolver implementation.
 func (r *Resolver) Contract() generated.ContractResolver { return &contractResolver{r} }
+
+// Mutation returns generated.MutationResolver implementation.
+func (r *Resolver) Mutation() generated.MutationResolver { return &mutationResolver{r} }
 
 // Query returns generated.QueryResolver implementation.
 func (r *Resolver) Query() generated.QueryResolver { return &queryResolver{r} }
 
 type contractResolver struct{ *Resolver }
+type mutationResolver struct{ *Resolver }
 type queryResolver struct{ *Resolver }
